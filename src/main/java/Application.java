23504de300235386fd18b47c7692c160ccdd4b90
Пунь-Пунь <wows15190305@mail.ru>
2021-11-05 07:00:17 +0300

    .

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         double x;
         double y;
-        String action = args [1];
+        String action = args [0];
 
         Scanner input = new Scanner(System.in);
 
@@ -17,21 +17,22 @@ public class Application {
         Scanner op = new Scanner(System.in);
 
         System.out.println("Введите операцию");
-        args [1] = op.next();
+        args [0] = op.next();
 
-        if (args [1].equals("+")) { // сложение
+        if (args [0].equals("+")) { // сложение
             System.out.println("Ответ: " + (x + y));
-        } else if (args [1].equals("-")) { // вычетание
+        } else if (args [0].equals("-")) { // вычетание
             System.out.println("Ответ: " + (x - y));
-        } else if (args [1].equals("/")) { //деление
+        } else if (args [0].equals("/")) { //деление
             System.out.println("Ответ " + (x/ y));
-        } else if (args [1].equals("*")) { // умножение
+        } else if (args [0].equals("*")) { // умножение
             System.out.println("Ответ: " + (x * y));
-        } else if (args [1].equals("%")) { // процент от числа
+        } else if (args [0].equals("%")) { // процент от числа
             System.out.println("Ответ: " + (x % y));
-        } else if (args [1].equals("^")) { // возведение в степень
+        } else if (args [0].equals("^")) { // возведение в степень
             System.out.println(Math.pow(x,y));
+        } if(args[0].equals ("/") && y == 0)  // деление на ноль
+            System.out.println ("Деление на ноль невозможно");
 
         }
-    }
     }
