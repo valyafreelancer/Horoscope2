8f4000f4263ba6b702d9eed9bd675c0aa0120260
Пунь-Пунь <wows15190305@mail.ru>
2021-11-08 09:51:59 +0300

    .

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,35 +3,46 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Введите первое число");
-        double x= input.nextDouble();
-
-        System.out.println("Введите второе число");
-        double y= input.nextDouble();
-
-        String action = args [0];
-
-
         Scanner op = new Scanner(System.in);
 
-        System.out.println("Введите операцию");
-        args [0] = op.next();
+        System.out.println("Введите первое число");
+        double x = input.nextDouble();
 
-        if (args [0].equals("+")) { // сложение
-            System.out.println("Ответ: " + (x + y));
-        } else if (args [0].equals("-")) { // вычетание
-            System.out.println("Ответ: " + (x - y));
-        } else if (args [0].equals("/")) { //деление
-            System.out.println("Ответ " + (x/ y));
-        } else if (args [0].equals("*")) { // умножение
-            System.out.println("Ответ: " + (x * y));
-        } else if (args [0].equals("%")) { // процент от числа
-            System.out.println("Ответ: " + (x % y));
-        } else if (args [0].equals("^")) { // возведение в степень
-            System.out.println(Math.pow(x,y));
-        } if(args[0].equals ("/") && y == 0)  // деление на ноль
-            System.out.println ("Деление на ноль невозможно");
+        System.out.println("Введите второе число");
+        double y = input.nextDouble();
+
+
+        System.out.println("Введите операцию");
+        String action = input.next();
+
+        switch (action) {
+            case "+":
+                System.out.println("Ответ: " + (x + y));
+                break;
+            case "-":
+                System.out.println("Ответ: " + (x - y));
+                break;
+            case "/":
+                System.out.println("Ответ: " + (x / y));
+                if (y == 0) {
+                    throw new IllegalArgumentException("Деление на ноль невозможно!");
+                }
+                break;
+            case "*":
+                System.out.println("Ответ: " + (x * y));
+                break;
+            case "%":
+                System.out.println("Ответ: " + (x % y));
+                break;
+            case "^":
+                System.out.println("Ответ: " + Math.pow(x,y));
+                break;
+            case "":
+                System.out.println("Ошибка!");
+                break;
+
 
         }
+    }
+
     }
